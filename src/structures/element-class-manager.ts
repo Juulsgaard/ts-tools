a05cf6e60914b46c7102c56ce74b20b893669dd7
Frederik Sørensen <frederikjs1@hotmail.com>
2024-02-23 15:36:56 +0100

    Add JS Docs to ElementClassManager

--- src/structures/element-class-manager.ts
+++ src/structures/element-class-manager.ts
@@ -2,6 +2,10 @@ import {isArray, isString} from "../helpers/type-predicates";
 
 export type ElementClasses = string[] | Record<string, boolean> | string | undefined | null;
 
+/**
+ * Unify different class collection formats into a list of strings
+ * @param classes - The class collection
+ */
 export function processElementClasses(classes: ElementClasses): string[] {
   if (classes == null) return [];
   if (isString(classes)) return [classes];
@@ -15,6 +19,10 @@ export function processElementClasses(classes: ElementClasses): string[] {
   return out;
 }
 
+/**
+ * A class to manage the applied classes on an HTML Element.
+ * The class keeps track of what classes were added and can use that to create differentials.
+ */
 export class ElementClassManager<T extends HTMLElement = HTMLElement> {
   private readonly _classes = new Set<string>();
   get classes(): ReadonlySet<string> {
@@ -26,6 +34,11 @@ export class ElementClassManager<T extends HTMLElement = HTMLElement> {
     this.element.classList.add(...this.classes);
   }
 
+  /**
+   * Update the applied classes to the given state.
+   * New classes will be applied, and classes not in the new state will be removed.
+   * @param classes - The new classes state
+   */
   set(classes: ElementClasses) {
     const current = new Set(this.classes);
     const toSet = processElementClasses(classes);
@@ -43,6 +56,11 @@ export class ElementClassManager<T extends HTMLElement = HTMLElement> {
     }
   }
 
+  /**
+   * Add classes to the HTML Element.
+   * @param classes - The classes to add
+   * @returns addedClasses - A list of classes that were added to the state
+   */
   add(classes: ElementClasses): string[] {
     const toAdd = processElementClasses(classes);
     const added: string[] = [];
@@ -57,6 +75,11 @@ export class ElementClassManager<T extends HTMLElement = HTMLElement> {
     return added;
   }
 
+  /**
+   * Remove classes from the HTML Element.
+   * @param classes - The classes to remove
+   * @returns removedClasses - A list of classes that were removed from the state
+   */
   remove(classes: ElementClasses): string[] {
     const toRemove = processElementClasses(classes);
     const removed: string[] = [];
